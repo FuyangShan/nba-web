d508758bf404d85deb13e36334b9dfa1c5edfba0
FuyangShan <40450679+FuyangShan@users.noreply.github.com>
2020-02-09 15:48:55 -0800

    Visulize the player info

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
+import { PROFILE_PIC_URL_PREFIX, TEAM_PIC_URL_PREFIX } from '../constants';
 
 class Profile extends Component {
     render() {
+        const {
+            teamAbbreviation,
+            teamCity,
+            teamName,
+            playerName,
+            height,
+            weight,
+            playerId,
+            pts, reb, ast, pie,
+        } = this.props.playerInfo;
+
         return (
-            <div>
-                profile
+            <div className="profile">
+                <div className="profile-entry player-name">{`${playerName}`}</div>
+                <img
+                    className="profile-pic"
+                    src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}
+                    alt="Profile"
+                />
             </div>
         );
     }
