1a52b6316b40ecd80062c7a0e3b1d26143b2338f
FuyangShan <40450679+FuyangShan@users.noreply.github.com>
2020-02-09 15:56:05 -0800

    Add more player info and style the profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -22,6 +22,40 @@ class Profile extends Component {
                     src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}
                     alt="Profile"
                 />
+                <div className="profile-entry">
+                    <div className="profile-entry-left">Team</div>
+                    <div className="profile-entry-right">{`${teamCity} ${teamName}`}</div>
+                </div>
+                <img
+                    className="team-logo"
+                    src={`${TEAM_PIC_URL_PREFIX}/${teamAbbreviation}_logo.svg`}
+                    alt="Team"
+                />
+                <div className="profile-entry">
+                    <div className="profile-entry-left">Height</div>
+                    <div className="profile-entry-right">{`${height}`}</div>
+                </div>
+                <div className="profile-entry">
+                    <div className="profile-entry-left">Weight</div>
+                    <div className="profile-entry-right">{`${weight}`}</div>
+                </div>
+                <div className="profile-entry">
+                    <div className="profile-entry-left">PTS</div>
+                    <div className="profile-entry-right">{`${pts}`}</div>
+                </div>
+                <div className="profile-entry">
+                    <div className="profile-entry-left">REB</div>
+                    <div className="profile-entry-right">{`${reb}`}</div>
+                </div>
+                <div className="profile-entry">
+                    <div className="profile-entry-left">AST</div>
+                    <div className="profile-entry-right">{`${ast}`}</div>
+                </div>
+                <div className="profile-entry">
+                    <div className="profile-entry-left">PIE</div>
+                    <div className="profile-entry-right">{`${pie}`}</div>
+                </div>
+
             </div>
         );
     }
